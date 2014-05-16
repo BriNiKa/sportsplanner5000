5867d762fbeaf5fb8488ac247823ee23065c090e
kaufsim <simon@hbkaufmann.ch>
2014-05-16 15:49:12 +0200

    EventController mit Hauptmethoden --> doCreate / doUpdate / doDelete EventEJB mit Hauptmethoden PlaceEJB korrigiert

--- src/main/java/ch/brinika/sportsplanner5000/controller/EventController.java
+++ src/main/java/ch/brinika/sportsplanner5000/controller/EventController.java
@@ -6,16 +6,68 @@
 
 package ch.brinika.sportsplanner5000.controller;
 
+import ch.brinika.sportsplanner5000.business.EventEJB;
+import ch.brinika.sportsplanner5000.domain.Event;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+
 /**
  *
  * @author simon
  */
 public class EventController {
 
-    /**
-     * Creates a new instance of EventController
-     */
-    public EventController() {
+    private Event event = new Event();
+    private List<Event> eventList  = new ArrayList<Event>();
+    
+    @EJB
+    private EventEJB eventEJB;
+
+    public String doCreateEvent()
+    {
+        eventEJB.createEvent(event);
+        eventList = eventEJB.findEvents();
+        return "Event_Created";
+    }
+    
+     public String doUpdateEvent()
+    {
+        eventEJB.updateEvent(event);
+        eventList = eventEJB.findEvents();
+        return "Event_updated";
+    }
+     
+       public String doDeleteEvent()
+    {
+        eventEJB.deleteEvent(event);
+        eventList = eventEJB.findEvents();
+        return "Event_deleted";
+    }
+    
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event  event) {
+        this.event = event;
+    }
+
+    public List<Event> getEventList() {
+        eventList = eventEJB.findEvents();
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    public EventEJB getEventEJB() {
+        return eventEJB;
+    }
+
+    public void setEventEJB(EventEJB eventEJB) {
+        this.eventEJB = eventEJB;
     }
     
 }
