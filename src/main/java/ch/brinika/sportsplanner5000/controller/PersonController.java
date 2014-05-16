b45c79f22db8cc8e08323145c73aca19371f8657
kaufsim <simon@hbkaufmann.ch>
2014-05-16 11:03:34 +0200

    Controller --> JSF ManagedBean EJB --> Session Bean

---
+++ src/main/java/ch/brinika/sportsplanner5000/controller/PersonController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.brinika.sportsplanner5000.controller;
+
+/**
+ *
+ * @author simon
+ */
+public class PersonController {
+
+    /**
+     * Creates a new instance of PersonController
+     */
+    public PersonController() {
+    }
+    
+}
---
+++ src/main/java/ch/brinika/sportsplanner5000/controller/TeamController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.brinika.sportsplanner5000.controller;
+
+/**
+ *
+ * @author simon
+ */
+public class TeamController {
+
+    /**
+     * Creates a new instance of TeamController
+     */
+    public TeamController() {
+    }
+    
+}
